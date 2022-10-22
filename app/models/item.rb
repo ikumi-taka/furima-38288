2e2990dab585c29f1fdf93da5563489898508e6d
ikumi-taka <canomori0311@gmail.com>
2022-10-22 15:43:27 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,25 @@ class Item < ApplicationRecord
   belongs_to :shipping_time
 
   has_one_attached :image
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do  #選択肢「---」が選ばれているときは保存しない
+    validates :category
+    validates :item_status
+    validates :shipping_cost
+    validates :shipping_from
+    validates :shipping_time
+  end
+  
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :shipping_from_id
+    validates :shipping_time_id
+    validates :user_id
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
 end
