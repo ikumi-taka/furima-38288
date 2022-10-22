db18445f6a491bf4e5c419da99da1553ddfc542e
ikumi-taka <canomori0311@gmail.com>
2022-10-22 13:51:45 +0900

    ActiveHachモデル作成/アソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHAsh::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_from
+  belongs_to :shipping_time
+
   has_one_attached :image
 end
