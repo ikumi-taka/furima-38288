4c1ff80ae5922c7830fa42277de0db55787eecc7
ikumi-taka <canomori0311@gmail.com>
2022-10-28 13:05:24 +0900

    販売手数料/利益の計算が表記されるよう記述

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,12 @@
 window.addEventListener("load", () => {
-  console.log("OK");
+
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  console.log(inputValue);
+  const addTaxDom = document.getElementById("add-tax-price");
+  const addProfitDom = document.getElementById("profit")
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+  addProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+  })
 });
