a73f500715e2499cf4f5ccfdd90b351423dd709f
ikumi-taka <canomori0311@gmail.com>
2022-10-31 21:21:41 +0900

    コードレビュー後修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_30_045915) do
+ActiveRecord::Schema.define(version: 2022_10_20_024532) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
