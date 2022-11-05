d5017f8311b42f85a2f7e634cefac26fffa190a4
ikumi-taka <canomori0311@gmail.com>
2022-11-05 14:13:23 +0900

    重複した記述をまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] # ログインしていないユーザーをログインページの画面に促す
+  before_action :set_item, only: [:show, :edit, :update] #重複した記述をまとめる
+
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,18 +21,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id #ログイン中、他の出品者の編集ページに直接遷移しようとするとトップへ遷移する
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(items_params)
       redirect_to item_path
     else
@@ -43,5 +42,9 @@ class ItemsController < ApplicationController
   def items_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id,
                                  :shipping_from_id, :shipping_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
