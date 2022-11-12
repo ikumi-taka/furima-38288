0150f04491a860fc2102f7f2f01a16e29e8dc735
ikumi-taka <canomori0311@gmail.com>
2022-11-12 16:01:49 +0900

    ordersコントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_address)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def order_address
+    params.require(:order_address).permit(:post_code, :shipping_from_id, :city, :block_number, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
+
