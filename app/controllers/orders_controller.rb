e4af867f37b1fe08be920b29d7c5a45d037cc979
ikumi-taka <canomori0311@gmail.com>
2022-11-13 09:01:01 +0900

    購入画面商品名等表示・テストファイルの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
