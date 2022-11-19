a66e04ad0c6866f6aaf00baee054b432ea31534d
ikumi-taka <canomori0311@gmail.com>
2022-11-19 16:59:09 +0900

    不要なコードの削除

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -6,8 +6,6 @@ FactoryBot.define do
     block_number     { Faker::Address.street_address }
     building         { Faker::Address.street_address }
     phone_number     { Faker::Number.decimal_part(digits: 11) }
-    user_id          { Faker::Number.non_zero_digit }
-    item_id          { Faker::Number.non_zero_digit }
     token            { Faker::Internet.password(min_length: 20, max_length: 30) }
   end
 end
