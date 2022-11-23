3bfbcb5e07ccc4eceb60554cc4af88e9c766f8d3
ikumi-taka <canomori0311@gmail.com>
2022-11-23 22:16:54 +0900

    自動デプロイ修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.1'
 set :application, 'furima-38288'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:ikumi/furima-38288.git'
+set :repo_url,  'git@github.com:ikumi-taka/furima-38288.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
