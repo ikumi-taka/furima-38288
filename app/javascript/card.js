96202966d36681165d7d1a2b8cff8df8730b1c8d
ikumi-taka <canomori0311@gmail.com>
2023-01-08 16:09:13 +0900

    payjpv2に修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,37 +1,32 @@
 const pay = () => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY); //PAY.JPテスト公開鍵
-  const submit = document.getElementById("button"); //buttonというidを指定している
-  submit.addEventListener("click", (e) => {   //購入ボタンをクリックするとイベント発火
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY); //PAY.JPテスト公開鍵
+  const elements = payjp.elements();//elementsインスタンスを生成
+
+  //入力欄ごとにelementインスタンスを生成
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  //入力欄をブラウザ上に表示
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  //入力フォームの要素を取得
+  const submit = document.getElementById("button");
+  submit.addEventListener("click", (e) => {
     e.preventDefault();
-
-    const formResult = document.getElementById("charge-form");
-    const formData = new FormData(formResult);
-
-    const card = {
-      number: formData.get("order_address[number]"),    //クレカ情報を取得して変数cardに代入
-      exp_month: formData.get("order_address[exp_month]"),
-      exp_year: `20${formData.get("order_address[exp_year]")}`,
-      cvc: formData.get("order_address[cvc]"),
-    };
-
-    Payjp.createToken(card, (status, response) => {
-      if (status == 200) {
-        //トークンの値をフォームに含める
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden"> `;  //トークンの値を非表示にするためhidden追記
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-
-      //フォームに存在するクレカ情報を削除する
-      document.getElementById("card-number").removeAttribute("name");
-      document.getElementById("card-exp-year").removeAttribute("name");
-      document.getElementById("card-exp-month").removeAttribute("name");
-      document.getElementById("card-cvc").removeAttribute("name");
-
-      document.getElementById("charge-form").submit();  //フォームの情報をサーバーサイドに送信
+      document.getElementById("charge-form").submit();
     });
-  });
-};
+  })
+}
 
 window.addEventListener("load", pay);
