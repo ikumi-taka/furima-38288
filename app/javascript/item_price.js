5e31abe15e11e38da621a8270ca41c02afc764fd
ikumi-taka <canomori0311@gmail.com>
2023-01-08 16:06:18 +0900

    エラーの修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,7 @@
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
 
 const priceInput = document.getElementById("item-price");
+if (!priceInput){ return false;}
 priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;  //value属性を指定し、input要素から実際に入力した金額の値を取得
   const addTaxDom = document.getElementById("add-tax-price");
